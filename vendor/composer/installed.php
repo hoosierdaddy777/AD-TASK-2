6b185e79caacee4e7cb5d3cc83123965dd125739
Carlo Dulutan <202310776@fit.edu.ph>
2025-05-20 04:43:14 -0700

    refactor(composer.lock) Composer install which provided composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'carlo-dulutan/ad-task-2',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '461d0cd757c5d98de7eebdf8c587aa755af2f13e',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'carlo-dulutan/ad-task-2' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '461d0cd757c5d98de7eebdf8c587aa755af2f13e',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
